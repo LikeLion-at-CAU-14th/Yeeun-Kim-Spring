7edb5bd6f0c3c10b2ca7204e66957b1cd2bdbcf1
김예은 <lpdms12@gmail.com>
2026-07-19 13:05:13 +0900

    feat: SpringBoot 세션 실습

---
+++ src/main/java/com/example/likelion14th_springboot/Likelion14thSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion14th_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion14thSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion14thSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/likelion14th_springboot/Likelion14thSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion14th_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion14thSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
